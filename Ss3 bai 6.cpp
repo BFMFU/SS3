37b1ddf4b01335f75c69927beba9828fe02a407e
Nguyen Duy Manh <manhduy0989@gmail.com>
2024-11-21 22:59:07 +0700

    Add files via upload

---
+++ Ss3 bai 6.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    float edge, height, area;
+
+    printf("Nhap do dai canh day cua tam giac: ");
+    scanf("%f", &edge);
+    printf("Nhap chieu cao cua tam giac: ");
+    scanf("%f", &height);
+    area = 0.5 *edge*height;
+
+    printf("Dien tich cua tam giac la: %.2f\n", area);
+
+    return 0;
+}
+
